7d073bc354ff1f9ad7fcec5587fb1df51a7008af
Josh Herrera <joshherrera09@gmail.com>
2020-11-23 14:57:47 -0800

    jumbotron component added

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1><span><a className="white" id="jumbo" href="/">Enjoy Your Book Search</a></span></h1>
+            <div>
+                <a className="white" target="_blank" href="http://developers.google.com/books/">
+                    Powered by Google Books
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
